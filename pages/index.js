4c20c60f68f5ff9bffb52345d203c3c8177f7af4
Dair Smith <dair.code@gmail.com>
2022-08-18 02:18:59 -0500

    reusable loader and navbar that navs to pages

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,12 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 
+import Loader from '../components/Loader';
+
 export default function Home() {
   return (
     <div>
-      <Link prefetch={false} href={{
-        pathname: '/[username]',
-        query: { username: 'bogusrealms' },
-      }}>
-        <a>Dair's Profile</a>
-      </Link>
+      <Loader show />
     </div>
   );
 }
