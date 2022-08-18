81743a7e74ff8f3b168a9ab1c69a3df20c388452
Dair Smith <dair.code@gmail.com>
2022-08-18 15:09:00 -0500

    auth, context, usernames, debounce, etc

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,18 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useContext } from "react";
+import { UserContext } from "../lib/context";
+import { auth } from "../lib/firebase";
 
 export default function Navbar() {
-    const user = null;
-    const username = null;
+    const { user, username } = useContext(UserContext);
+
+    const router = useRouter();
+
+    const signOut = () => {
+        auth.signOut();
+        router.reload();
+    }
 
     return (
         <nav className="navbar">
@@ -17,13 +27,16 @@ export default function Navbar() {
                 {username && (
                     <>
                         <li className="push-left">
+                            <button onClick={signOut}>Sign Out</button>
+                        </li>
+                        <li>
                             <Link href="/admin">
                                 <button className="btn-blue">Write Posts</button>
                             </Link>
                         </li>
                         <li>
                             <Link href={`/${username}`}>
-                                <img src={user?.photoURL} />
+                                <img referrerPolicy="no-referrer" src={user?.photoURL || './hacker.png'} />
                             </Link>
                         </li>
                     </>
